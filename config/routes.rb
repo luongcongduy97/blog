f26dbf6ef2bec19f5531b14c691ada243f22cded
luongcongduy97 <duylcong97@gmail.com>
2025-02-11 16:51:07 +0700

    add authenticate

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  resource :session
+  resources :passwords, param: :token
   resources :posts do
     resources :comments
   end
@@ -13,5 +15,5 @@ Rails.application.routes.draw do
   # get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
 
   # Defines the root path route ("/")
-  # root "posts#index"
+  root "posts#index"
 end
